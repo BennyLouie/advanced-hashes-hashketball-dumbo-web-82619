e5527ed24fb0beafcb232dea82424c0a0eded596
Benny Louie <bennylouie1412@gmail.com>
2019-08-13 18:55:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,9 +194,9 @@ def big_shoe_rebounds
     stats[:players].each do |i_player|
       i_player.each do |name, values|
         shoes.push(i_player[name][:shoe])
-        if i_player[name][:shoe] == shoes.max
-          return i_player[name][:rebounds]
-        end
+        # if i_player[name][:shoe] == shoes.max
+        #   return i_player[name][:rebounds]
+        # end
       end
     end
   end
