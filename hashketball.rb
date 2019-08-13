d04b30983b52c9f6a7c30472d9a189972998cbb9
Benny Louie <bennylouie1412@gmail.com>
2019-08-13 17:48:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -158,12 +158,12 @@ def player_numbers(team)
   game_hash.each do |teams, stats|
     if stats[:team_name] == team 
       stats[:player].each do |player|
-        binding.pry
         jersey_numbers.push(player[:number])
       end
     end
   end
   jersey_numbers
+  binding.pry
 end
 
 
