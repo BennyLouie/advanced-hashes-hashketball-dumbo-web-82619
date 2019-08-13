b891acb1921067d950dac4da849054fcde6261aa
Benny Louie <bennylouie1412@gmail.com>
2019-08-13 17:47:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,6 @@ end
 def shoe_size(player)
   game_hash.each do |team, stats|
     stats[:players].each do |i_player|
-      binding.pry
       if i_player[:player_name] == player 
         return i_player[:shoe]
       end
