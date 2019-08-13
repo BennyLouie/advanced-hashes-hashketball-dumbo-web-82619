06d660f828a6cde25a4d90a4183e478e6de4ae87
Benny Louie <bennylouie1412@gmail.com>
2019-08-13 18:02:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,7 @@ def num_points_scored(player)
   game_hash.each do |team, stats|
     stats[:players].each do |i_player|
       binding.pry
-      if i_player[:player_name] == player 
+      if i_player[player]
         return i_player[:points]
       end
     end
