c495995d30380c09dbf76f8bd955449fa4772799
Benny Louie <bennylouie1412@gmail.com>
2019-08-13 19:14:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -239,7 +239,6 @@ points = []
   game_hash.each do |team, stats|
     stats[:players].each do |i_player|
       i_player.each do |name, values|
-        binding.pry
         if i_player[name][:points] == points.max
           return stats[:team_name]
         end
